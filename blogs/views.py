a053510cb26a6291edd16b2fa71f999b6750a6de
Leandro Liset <leandroliset@gmail.com>
2023-08-12 03:25:21 -0300

    Primera version del proyecto final

---
+++ blogs/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from blogs.models import Post, Category
+
+# Create your views here.
+
+def home_page(request):
+    posts = Post.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        'posts' : posts,
+        'categories' : categories,
+    }
+    return render(request, 'blogs/home_page.html', context=context)
