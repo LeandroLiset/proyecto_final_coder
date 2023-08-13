6dd2b3485beaaaa3ed89eb77d31189851e4e96a7
Leandro Liset <leandroliset@gmail.com>
2023-08-13 00:37:37 -0300

    Segunda version del proyecto final, agregando urls, vistas y templates

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,8 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
 from blogs.models import Post, Category
 
 # Create your views here.
@@ -7,9 +10,21 @@ from blogs.models import Post, Category
 def home_page(request):
     posts = Post.objects.all()
     categories = Category.objects.all()
+    featured = Post.objects.filter(featured=True)[:3]
 
     context = {
-        'posts' : posts,
+        'post_list' : posts,
         'categories' : categories,
+        'featured' : featured,
     }
+
     return render(request, 'blogs/home_page.html', context=context)
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
