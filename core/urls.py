b8fada59fd8d4a8dc93cd0dc1ffefd9c75d5dcc4
Leandro Liset <leandroliset@gmail.com>
2023-08-14 22:21:20 -0300

    Agregada funcionalidad a crear cuenta

--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('django.contrib.auth.urls')),
-    path('', include('blogs.urls'))
+    path('', include('blogs.urls')),
+    path('', include('users.urls'))
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
